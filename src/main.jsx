696cbe4654240d85fc4aa2b8358cf2fe1e69f78e
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-05-24 19:48:00 +0330

    add the last lesson

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+import "./index.css";
+
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Root, { loader as rootLoader } from "./routes/root";
+
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import Root, {
+   action as rootAction,
+   loader as rootLoader,
+} from "./routes/root";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
    {
@@ -12,10 +19,18 @@ const router = createBrowserRouter([
       element: <Root />,
       errorElement: <ErrorPage />,
       loader: rootLoader,
+      action: rootAction,
       children: [
          {
             path: "contacts/:contactId",
             element: <Contact />,
+            loader: contactLoader,
+         },
+         {
+            path: "contacts/:contactsId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
          },
       ],
    },
